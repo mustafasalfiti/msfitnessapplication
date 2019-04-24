fc6a4182678e143d6056caf569d628430b06b88c
mustafasalfiti <mmsalfiti@gmail.com>
2019-04-24 14:41:04 +0200

    auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const User = require('../models/User');
+
+
+module.export = {
+  async requireLogin(req , res , next) {
+    let token = req.signedCookies['alpha-access'] || req.signedCookies['omega-access'] || undefined;
+    if(token) {
+      const { _id }= jwt.verify(token , 'key12345');
+      const user = await User.findOne({_id} , '-password -_id');
+      req.user = user;
+      next();
+    } else {
+      return res.status(401).send("You need Permission to do that");
+    }
+ }
+,  
+
+  requireAdmin(req , res , next) {
+    if(req.user.type === 'admin') {
+      next();
+    } else {
+      return res.status(403).send("You dont have permission to do that")
+    }
+
+  }
+}
